94af5aa8bce8e85b341a7925c3c1207d928bf0ff
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-29 09:43:54 -0600

    Update homepage welcome message styling and responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ const HomePage = async () => {
 
   return (
     <main className='flex flex-col h-screen text-center bg-gray-100'>
-      <h1 className='text-4xl mt-20 mb-4'>Welcome to your Dashboard</h1>
+      <h1 className='text-2xl lg:text-4xl mt-20 mb-4'>Welcome to your Dashboard</h1>
       <div className='container mx-auto'>
-        <p className='text-lg my-4'>You are signed in as: <span className='font-bold'>{user.fullName}</span></p>
+        <p className='lg:text-lg my-4'>You are signed in as: <span className='font-bold'>{user.fullName}</span></p>
       </div>
     </main>
   );
