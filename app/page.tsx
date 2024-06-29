188450e9acfa884153954c90681996afc2872853
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-29 11:36:27 -0600

    chore: Update homepage layout and add Dashboard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { currentUser } from '@clerk/nextjs/server';
 import SignIn from '@/components/SignIn';
+import Dashboard from '@/components/Dashboard';
 
 const HomePage = async () => {
   const user = await currentUser();
@@ -9,12 +10,19 @@ const HomePage = async () => {
   }
 
   return (
-    <main className='flex flex-col h-screen text-center bg-gray-100'>
-      <h1 className='text-2xl lg:text-4xl mt-20 mb-4'>Welcome to your Dashboard</h1>
-      <div className='container mx-auto'>
-        <p className='lg:text-lg my-4'>You are signed in as: <span className='font-bold'>{user.fullName}</span></p>
+    <>
+    <div className='h-screen justify-center text-center lg:text-center lg:justify-between md:text-center md:justify-between px-14 py-6 bg-gray-100'>
+      <h1 className='text-xl lg:text-2xl md:text-xl'>Dashboard</h1>
+        <p className='lg:text-md md:text-sm max-sm:hidden lg-block'>You are signed in as: <span className='font-bold'>{user.fullName}</span></p>
+        <div className='grid lg:grid-cols-3 md:grid-cols-2 justify-between text-center mt-8'>
+          <Dashboard />
+          <Dashboard />
+          <Dashboard />
+          </div>
       </div>
-    </main>
+      
+
+      </>
   );
 }
 
